0acd2be35c1e4083576877654299f15a2ba6d84f
fatso1000 <agustinbenitez81@gmail.com>
2021-02-28 21:05:59 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import Header from "./components/header";
+import "./App.css";
 
 function App() {
+  const [darkMode, setDarkMode] = React.useState(false);
+
+  React.useEffect(() => {
+    darkMode
+      ? document.body.classList.add("dark")
+      : document.body.classList.remove("dark");
+  }, [darkMode]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="fullsize">
+        {/* <Header /> */}
+        <div className="container">
+          <div name="presentation">
+            <h1 className="title">PORTFOLIO</h1>
+            <p className="subtitle">by Matias</p>
+          </div>
+          <div className="container-2">
+            <a href="#" className="btn-text">
+              <img src="chevron-down-solid.svg" alt="scroll down button" />
+              Scroll
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="fullsize">
+        <div className="container">
+          <div className="presentation">
+            <h1 className="title">WHO I AM</h1>
+            <p className="subtitle">
+              My name is Matias and i'm a <bold>Full-Stack Developer</bold>.
+              Also i'm 19 years old and currently living in Buenos Aires,
+              Argentina. I've a degree as <bold>Programming Technician</bold>{" "}
+              and currently i'm studying on{" "}
+              <bold>
+                <a href="https://www.soyhenry.com/" className="hyperlink">
+                  Henry
+                </a>
+              </bold>
+              .
+            </p>
+            <button onClick={() => setDarkMode(!darkMode)} className="btn-dark">
+              Dark Mode
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="fullsize">
+        <div className="container">
+          <div className="presentation-projects">
+            <h1 className="title">SOME PROJECTS</h1>
+            <div className="list-container">
+              <ul>
+                <li>
+                  <p className="subtitle">
+                    <bold>xkcd project</bold> - Front-end project
+                  </p>
+                </li>
+                <li>
+                  <p className="subtitle">
+                    <bold>jodapp project</bold> - Back-end project
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/header/index.js
@@ -0,0 +1,9 @@
+import "./Header.css"
+
+export default function Header() {
+    return (
+        <nav className="navbar">
+            <h1>NAVBAR</h1>
+        </nav>
+    );
+}
